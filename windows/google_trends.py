86367d188d81e7330ceee4ac43a0ec40ea264deb
alex kumar <alexander.kumar@duke.edu>
2022-08-10 14:43:09 -0400

    mac and windows folders

---
+++ windows/google_trends.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import utils
+
+def main():
+    dir_path = os.path.dirname(sys.executable)
+
+    timeframe, keyword, name = utils.get_all_inputs()
+
+    states = utils.makeDirs(dir_path, name)
+    
+    logfile = []
+
+    for state in states:
+
+        log = utils.stateReq(keyword, state, timeframe, dir_path, name)
+        logfile.append(log)
+
+        if state != 'PR':
+            metros = utils.getMetros(keyword, state, timeframe)
+
+            for m in metros:
+                log = utils.DMAReq(keyword, state, m, timeframe, dir_path, name) 
+                logfile.append(log)
+        
+    utils.saveLog(logfile, dir_path, name)
+
+    utils.megaCSV(path = dir_path + '/' + name + '/Keyword' + '/State')
+    utils.megaCSV(path = dir_path + '/' + name + '/Keyword' + '/DMA')
+
+    utils.done(dir_path, name)
+
+
+# ask to search by topic or keyword
+
+if __name__ == "__main__":
+    main()
